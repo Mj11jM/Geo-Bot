dcf4914840ece917bf8e3baad50bd90b756bd1e1
Mj11jM <themj11jm@gmail.com>
2021-04-14 23:46:44 -0700

    More rero testing, actually going to bed this time

--- Commands/reaction_roles.js
+++ Commands/reaction_roles.js
@@ -4,11 +4,27 @@ const _ = require('lodash')
 pcmd(['manageRoles'], ['reaction', 'role', 'add'], ['reaction', 'roles', 'add'], ['rero', 'add'], async (ctx, ...args) => {
     const groups = _.chunk(args, 2)
     let pairs = []
+    let error = false
     groups.map(x => {
         let reroPair = {}
-        let first = x[0]
-        let firstRole = ctx.message.member.guild.roles.filter(y => y.name.toLowerCase() === first)
-        console.log(firstRole)
+        let role = ctx.message.member.guild.roles.filter(y => y.name.toLowerCase() === x[0])
+        let emoji = x[1]
+        if (role.length === 0) {
+            role = ctx.message.member.guild.roles.filter(y => y.name.toLowerCase() === x[1])
+            emoji = x[0]
+        }
+        if (role.length === 0) {
+            error = true
+            return ctx.reply(`No roles found using ${x[0]} and ${x[1]}. Please make sure you enter either the role name exactly, or the role ID.`, 'red')
+        }
+        reroPair.emoji = emoji
+        reroPair.role = role[0]
+        pairs.push(reroPair)
+
     })
-    await ctx.send(ctx.message.channel.id, `${groups.join(', ')}`)
+    console.log(pairs)
+    if (!error) {
+        await ctx.send(ctx.message.channel.id, `${groups.join(', ')}`)
+
+    }
 })
