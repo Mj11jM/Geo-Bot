c65a25b58cf30a054dada5b28cbdda93cd95d04d
Mj11jM <themj11jm@gmail.com>
2021-11-15 03:13:32 -0800

    Fix polls

--- modules/polls.js
+++ modules/polls.js
@@ -1,11 +1,11 @@
 const createPoll = async (ctx, args, unique) => {
-    let parts = ctx.msg.filter(x=> x && x !== '|')
-    let question = parts.shift()
+    let parts = ctx.msg.join(' ').split('|').filter(x=> x && x !== '|')
+    let question = parts.shift().trim()
     let optLength = parts.length
     if (optLength > 10 )
         return
     let opt = ``
-    parts.map((x, i) => opt += `${ctx.icons[i+1]}: ${x}\n`)
+    parts.map((x, i) => opt += `${ctx.icons[i+1]}: ${x.trim()}\n`)
     let embed = {
         author: {name: question},
         description: opt
